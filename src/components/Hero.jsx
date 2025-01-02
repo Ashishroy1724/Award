519519e775af71d152a5575eb677f6f0fd0d4f64
Ashishroy1724 <ashishroy78782@gmail.com>
2025-01-03 01:25:58 +0530

    Video and MiniVideo added fully and made button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -28,9 +31,11 @@ const Hero = () => {
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
         <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+              
+              {/* // Mini Video */}
                <video
                 ref={nextVideoRef}
-                src={getVideoSrc(currentIndex + 1)}
+                src={getVideoSrc(upcomingVideoIndex)}
                 loop
                 muted
                 id="current-video"
@@ -39,7 +44,40 @@ const Hero = () => {
                />
             </div>
         </div>
+            {/* Primary Video */}
+            <video 
+                ref={nextVideoRef}
+                src={getVideoSrc(currentIndex)} 
+                loop
+                muted
+                id="next-video"
+                className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            />
+            <video 
+                src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                autoPlay
+                loop
+                muted
+                className="absolute left-0 top-0 size-full object-cover object-center"
+                onLoadedData={handleLoadedVideo}
+            /> 
       </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">G<b>a</b>ming</h1>
+      <div className="absolute left-0 top-0 z-40 size-full">
+      <div className="mt-24 px-5 sm:px-10">
+        <h1 className="special-font hero-heading text-blue-100">Redefi<b>n</b><b>e</b></h1>
+        <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+            Enter the MetaVerse <br /> Unlesh the Play Economy
+        </p>
+        <Button
+              id="watch-trailer"
+              title="Watch trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
+      </div>
+      </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">G<b>a</b>ming</h1>
     </div>
   )
 }
